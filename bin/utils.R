0a71fd134fc703f792fcef14c3f641901b22e6fd
jow30 <qiaoshan@uchicago.edu>
2026-04-28 14:12:47 -0500

    add utils.R

---
+++ bin/utils.R
@@ -0,0 +1,6 @@
+get_script_dir <- function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  file_arg <- "--file="
+  script_path <- sub(file_arg, "", args[grep(file_arg, args)])
+  dirname(normalizePath(script_path))
+}
